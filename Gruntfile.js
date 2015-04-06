e785fdfe4dc886023ccf8139d13c636c6c05b42d
Andre Valle <andrevvalle@icloud.com>
2015-04-06 15:55:04 -0300

    config project

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,30 +1,65 @@
-var config = require('grunt-settings');
-
 module.exports = function(grunt) {
-	config.init(grunt);
 
-	// var jsFiles = [
-	// 	'vendor/assets/jquery/dist/jquery.js',
-	// 	'assets/javascripts/**/*.js'
-	// ];
-
-	config.set('connect', {
-		server : {
-			options : {
-				hostname : 'localhost',
-				port: 2222,
-				keepalive: true,
-				open: true,
-				base: 'public/'
+	grunt.initConfig({
+		connect :  {
+			server : {
+				options : {
+					hostname : 'localhost',
+					port: 9292,
+					keepalive: true,
+					open: true,
+					base: 'public/'
+				}
 			}
-		}     
-		
+		},
+		sass: {
+			dist: {
+				options: {
+					style: 'expanded',
+					sourcemap: 'none'
+				},
+				files: {
+					'development/assets/style/normalize.scss': 'vendor/assets/foundation/scss/normalize.scss',
+					'development/assets/style/grid.scss': 'vendor/assets/foundation/scss/foundation.scss',
+					'public/assets/style/main.css': 'development/assets/style/include.sass'
+				}
+			}
+		},
+		watch: {
+			gruntfile: {
+				files: ['Gruntfile.js']
+			},
+			sass: {
+				files: ['development/assets/style/*.sass'],
+				tasks: ['sass']
+			},
+			css: {
+				files: ['public/assets/style/*.css'],
+				options: {
+					livereload: true
+				}
+			},
+			html: {
+				files: ['public/*.html'],
+				options: {
+					livereload: true
+				}
+			}
+		}
 	});
 
-	config.registerTask('server', function(name){
-		grunt.log.ok('Success!');
-		grunt.log.header('Front End Carioca 2015 o//')
-		grunt.task.run(['connect']);
+	grunt.registerTask('default', function(){
+		grunt.task.run(['watch']);
 	});
+
+	grunt.registerTask('server', function(name){
+		grunt.task.run(['connect:server']);
+		grunt.log.ok('Opening Application: FRONT END CARIOCA - 2015');
+	});
+
+	grunt.loadNpmTasks('grunt-contrib-connect');
+	grunt.loadNpmTasks('grunt-contrib-sass');
+	grunt.loadNpmTasks('grunt-contrib-cssmin');
+	grunt.loadNpmTasks('grunt-contrib-watch');
 
 };
